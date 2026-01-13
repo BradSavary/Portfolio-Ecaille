4e77b6fb62a05cf9ca6476963afb5d49e3865b15
BradSavary <brad.savary@etu.unilim.fr>
2026-01-13 14:15:18 +0100

    refactor Projects component layout for improved responsiveness

--- app/components/Section/Projects.tsx
+++ app/components/Section/Projects.tsx
@@ -9,9 +9,9 @@ export default function Projects() {
     <section className=" py-10 px-6 md:px-12 lg:px-20 bg-grid" >
       <SectionTitle title="Mes projets phares" starColor="pink" borderColor="black"/>
       {/* Add project cards here*/}
-      <div className="flex flex-row gap-2 justify-center mt-15">
-        <div className="border p-40"><TitleProject1 className="w-40"/></div>
-        <div className="border p-40"><TitleProject2 className="w-40"/></div>
+      <div className="w-5/6 min-w-98 justify-self-center items-center flex flex-col md:flex-row gap-2 justify-center mt-15">
+        <div className="border w-1/2 flex justify-center aspect-2/1"><TitleProject1 className="w-40"/></div>
+        <div className="border w-1/2 flex justify-center aspect-2/1"><TitleProject2 className="w-40"/></div>
       </div>
     <div className="mt-10 flex flex-row items-center gap-1">
     <Star color="pink" className="h-10"/>
